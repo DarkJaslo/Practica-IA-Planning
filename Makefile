cf92e47065a5658747428164ed3828336e640058
Jon <jon.campillo@gmail.com>
2023-12-23 14:46:18 +0100

    Printea estructura base

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 pruebas:
-	g++ -O3 -Wall -D_GLIBCXX_DEBUG -o pruebas juegos-prueba.cpp
+	g++ -O3 -Wall -Wno-sign-compare -D_GLIBCXX_DEBUG -o pruebas juegos-prueba.cpp
+
+clean:
+	rm pruebas
--- juegos-prueba.cpp
+++ juegos-prueba.cpp
@@ -3,9 +3,50 @@
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+const vector<string> MESES = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+void printUniversalFacts()
+{
+    for(int i = 0; i < MESES.size()-1; ++i)
+    {
+        cout << "\t\t(inm_anterior " << MESES[i] << " " << MESES[i+1] << ")\n";
+    }
+
+	for(int i = 0; i < MESES.size(); ++i)
+	{
+        for(int j = i+1; j < MESES.size(); ++j)
+        {
+            cout << "\t\t(anterior " << MESES[i] << " " << MESES[j] << ")\n";
+        }
+	}
+}
 
 int main()
 {
-    cout << "test\n";
+    cout << "(define (problem Libros-base)\n";
+    cout << "\t(:domain Libros)\n";
+    cout << "\t(:objects\n";
+    cout << "\t\t"; 
+    //objetos a continuacion
+    cout << "\n";    //fin objetos
+    cout << "\t)\n"; //cierra parentesis objetos
+    cout << "\n";    //linea en blanco
+
+    cout << "\t(:init\n";
+
+    cout << "\t\t; Datos universales\n";
+	printUniversalFacts();
+    cout << "\t)\n";   //cierra init
+
+    cout << "\n"; //linea en blanco
+
+    cout << "\t(:goal\n";
+    cout << "\t\t; Para todos los libros que se quieren leer, asignados al plan\n";
+    cout << "\t\t(forall (?l - quiereL) (asignado ?l))\n";
+    cout << "\t)\n"; //cierra goal
+
+    cout << ")\n"; //cierra fichero
 }
