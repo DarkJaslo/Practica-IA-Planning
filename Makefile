1050339252012c7899c9c430e77fa65dc1a1595f
Jon <jon.campillo@gmail.com>
2023-12-30 13:58:11 +0100

    hace cosas el programa

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-pruebas:
+pruebas: juegos-prueba.cpp
 	g++ -O3 -Wall -Wno-sign-compare -D_GLIBCXX_DEBUG -o pruebas juegos-prueba.cpp
 
 clean:
--- juegos-prueba.cpp
+++ juegos-prueba.cpp
@@ -4,17 +4,144 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
 
 const vector<string> MESES = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 bool PRE_ON;
 bool PAR_ON;
 bool PAG_ON;
+int N;
+vector<pair<int,int>> PRES;
+vector<pair<int,int>> PARS;
+vector<int> PAGS;
+vector<vector<bool>> DAG;
+int PROB_PRE = 30;
+int PROB_PAR = 30;
+int PROB_LEER = 30;
+int SEED = 1234;
+mt19937 gen;
+uniform_int_distribution<int> distr;
+vector<bool> QUIERE_LEER;
 
+bool rollRandom(int prob)
+{
+    int randomN = distr(gen);
+    return randomN < prob;
+}
+
+void BFS(int nodo, vector<vector<bool>>& DAG, vector<vector<bool>>& reach)
+{
+    for(int i = nodo+1; i < N; ++i)
+    {
+        if(DAG[nodo][i])
+        {
+            reach[nodo][i] = true;
+
+            for(int j = 0; j < nodo; ++j)
+            {
+                if(reach[j][nodo])
+                {
+                    reach[j][i] = true;
+                    reach[i][j] = true;
+                }
+            }
+
+            reach[i][nodo] = true;
+        }
+    }
+}
+
+void creaDAG()
+{   
+    gen = mt19937(SEED);
+    distr = uniform_int_distribution<int>(0,99);
+
+    if(PAG_ON)
+        PAGS = vector<int>(N,0);
+    
+    DAG = vector<vector<bool>>(N,vector<bool>(N,false));
+
+    //Crea dependencias
+    if(PRE_ON)
+    {
+        for(int i = 0; i < N; ++i)
+        {
+            for(int j = i+1; j < N; ++j)
+            {            
+                if(rollRandom(PROB_PRE))
+                {
+                    DAG[i][j] = true;
+                    PRES.push_back(make_pair(i,j));
+                }  
+            }
+        }
+    }
+
+    //Entre libros sin dependencias entre medio, pon paralelos
+    if(PAR_ON)
+    {
+        //Matriz que indica si dos nodos estan conectados por algun camino
+        vector<vector<bool>> reach(N, vector<bool>(N,false));
+
+        for(int i = 0; i < N; ++i)
+        {
+            BFS(i,DAG,reach);
+        }
+
+        //Para los que no esten conectados, se puede poner paralelo
+        for(int i = 0; i < N; ++i)
+        {
+            for(int j = i+1; j < N; ++j)
+            {
+                if(not reach[i][j] and rollRandom(PROB_PAR))
+                {
+                    PARS.push_back(make_pair(i,j));
+                    PARS.push_back(make_pair(j,i));
+                }
+            }
+        }
+
+    }
+    
+    if(PAG_ON)
+    {
+        for(int i = 0; i < N; ++i)
+            PAGS[i] = distr(gen)+1;
+    }
+    else
+    {
+        for(int i = 0; i < N; ++i)
+            PAGS[i] = 0;
+    }
+
+    QUIERE_LEER = vector<bool>(N,false);
+
+    for(int i = 0; i < N; ++i)
+    {
+        if(rollRandom(PROB_LEER))
+        {
+            QUIERE_LEER[i] = true;
+        }
+    }
+}
 
 void printObjetos()
 {
+    for(int i = 0; i < N; ++i)
+    {
+        if(QUIERE_LEER[i])
+            cout << "Libro" << i << " "; 
+    }
+    cout << "- quiereL\n";
+    cout << "\t\t";
 
+    for(int i = 0; i < N; ++i)
+    {
+        if(not QUIERE_LEER[i])
+            cout << "Libro" << i << " "; 
+    }
+    cout << "- otrosL\n";
 }
 
 void printUniversalFacts()
@@ -31,32 +158,65 @@ void printUniversalFacts()
             cout << "\t\t(anterior " << MESES[i] << " " << MESES[j] << ")\n";
         }
 	}
+
+    for(int i = 0; i < MESES.size(); ++i)
+    {
+        cout << "\t\t(= (paginas-leidas " << MESES[i] << ") 0)\n";
+    }
 }
 
 void printRelaciones()
 {
+    if(PRE_ON)
+    {
+        for(int i = 0; i < PRES.size(); ++i)
+        {
+            cout << "\t\t(predecesor Libro" << PRES[i].first << " Libro" << PRES[i].second << ")\n";
+        }
+    }
 
+    if(PAR_ON)
+    {
+        for(int i = 0; i < PARS.size(); ++i)
+        {
+            cout << "\t\t(paralelo Libro" << PARS[i].first << " Libro" << PARS[i].second << ")";
+            cout << " (paralelo Libro" << PARS[i].second << " Libro" << PARS[i].first << ")\n";
+        }
+    }
+
+    for(int i = 0; i < N; ++i)
+    {
+        cout << "\t\t(= (paginas Libro" << i << ") " << PAGS[i] << ")\n";
+    }
 }
 
 void usage()
 {
-    cout << "Usage: ./juegos-prueba [prerequisitos] [paralelos] [paginas]\nDonde se pone \"on\" u \"off\" en cada campo, en funcion de si se quiere activar o no.\n";
+    cout << "Usage: ./juegos-prueba [N] [prerequisitos] [paralelos] [paginas]\nN es el numero de libros\nEn prerequisitos, paralelos y paginas, se pone \"on\" u \"off\" en cada campo, en funcion de si se quiere activar o no.\n";
 }
 
 int main(int argc, char** argv)
 {
-    if(argc != 4)
+    if(argc != 6)
     {
         usage();
     }
 
-    string pre = string(argv[1]);
-    string par = string(argv[2]);
-    string pag = string(argv[3]);
+    N = atoi(argv[1]);
+    string pre = string(argv[2]);
+    string par = string(argv[3]);
+    string pag = string(argv[4]);
 
     PRE_ON = pre=="on";
     PAR_ON = par=="on";
     PAG_ON = pag=="on";
+
+    cerr << "DEBUG: DAG con " << N << " libros:\n";
+    cerr << "prerequisitos "; PRE_ON ? cerr << "on" : cerr << "off"; cerr << "\n";
+    cerr << "paralelos "; PAR_ON ? cerr << "on" : cerr << "off"; cerr << "\n";
+    cerr << "npaginas "; PAG_ON ? cerr << "on" : cerr << "off"; cerr << "\n";
+
+    creaDAG();
 
     cout << "(define (problem Libros-base)\n";
     cout << "\t(:domain Libros)\n";
