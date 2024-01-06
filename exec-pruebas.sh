6bc8ab397a0aace33e9cf2356bc5ef086fd2bd2d
Jon <jon.campillo@gmail.com>
2024-01-06 14:05:21 +0100

    fix de path

--- exec-pruebas.sh
+++ exec-pruebas.sh
@@ -1,13 +1,13 @@
 # Ejecuta los juegos de prueba
 
 counter=10
-secs=20
+secs=15
 while [ $counter -le 40 ]
 do
     echo "---------------------------------------------------------"
     echo "ejecutando prueba $counter"
     echo "---------------------------------------------------------"
-    timeout $secs ff -O -o domain.pddl -f prueba$counter.pddl
+    timeout $secs ff -O -o domain.pddl -f ./juegos-prueba/prueba$counter.pddl
 
     if [ $? -eq 0 ]; then
         # Ha acabado a tiempo
