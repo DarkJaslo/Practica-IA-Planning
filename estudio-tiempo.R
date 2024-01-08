2bffe76b8a70440429d6236518fb4f93560c1eca
bunnerd <sandravaro03@gmail.com>
2024-01-08 21:01:10 +0100

    Ultimas cosas R

--- estudio-tiempo.R
+++ estudio-tiempo.R
@@ -5,7 +5,7 @@ PDDL <- read.table("resultados.txt", header = TRUE, sep = " ")
 #install.packages("RColorBrewer")
 library(RColorBrewer)
 palette <- brewer.pal(name="Set2", n=6)
-dev.new()
-plot(PDDL$tiempo ~ PDDL$libros, ylim=range(0, 12000), type="b",
-     col=palette[4], lwd=2, pch=22,
-     ylab="Tiempo", xlab="Número de libros")
+#dev.new()
+plot((PDDL$tiempo)/1000.0 ~ PDDL$libros, type="b", ylim=range(0,1),
+     lwd=1.5, pch=1,
+     ylab="Tiempo (s)", xlab="Número de libros")
